4d18695eaec8b4f1f1be43ce5dae143f4f3103c5
Ovais Sharif <sharifovais61@gmail.com>
2024-10-27 22:16:33 +0530

    started with header

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>This is the header</h1>
+
+      <div className='header_left'>
+        <img src='' alt=''/>
+        <div className='header_search'>
+            <SearchIcon/>
+            <input type='text'/>
+        </div>
+      </div>
+      <div className='header_right'></div>
+    </div>
+  )
+}
+
+export default Header
