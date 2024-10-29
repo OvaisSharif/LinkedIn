9792600d3cbe6185e1e9aebc130be16e6cdf0d4c
Ovais Sharif <sharifovais61@gmail.com>
2024-10-29 09:04:54 +0530

    created header components

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import linkedinIcon from "./images/linkedin-icon.svg" 
 
 function Header() {
   return (
     <div className='header'>
-      <h1>This is the header</h1>
-
       <div className='header_left'>
-        <img src='' alt=''/>
+        <img src={linkedinIcon} alt='linkedin-logo'/>
         <div className='header_search'>
             <SearchIcon/>
             <input type='text'/>
---
+++ src/HeaderOptions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HeaderOptions.css"
+
+function HeaderOptions() {
+  return (
+    <div className='header_options'>
+      
+    </div>
+  )
+}
+
+export default HeaderOptions
